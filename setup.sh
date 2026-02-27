feb103958e3cc1e6ff87a0727aaab099456b4d0b
Holm <you@example.com>
2026-02-27 01:39:22 +0000

    chore(all): add format.sh & setup.sh & cspec & commons-library & examples

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo "Instalando dependencias del sistema..."
+sudo apt update
+sudo apt install -y \
+    build-essential \
+    libreadline-dev \
+    libssl-dev \
+    clang-format \
+    git
+
+# CSpec
+echo "Instalando CSpec..."
+git clone https://github.com/mumuki/cspec.git /tmp/cspec
+sudo make -C /tmp/cspec install
+sudo rm -rf /tmp/cspec
+
+# Commons Library For C
+echo "Instalando so-commons-library..."
+git clone https://github.com/sisoputnfrba/so-commons-library.git /tmp/so-commons-library
+sudo make -C /tmp/so-commons-library install
+sudo rm -rf /tmp/so-commons-library
+
+# Update cache
+echo "Actualizando caché de librerías (ldconfig)..."
+sudo ldconfig
+
+echo "¡Configuración completada con éxito!"
